998b6d915a23cbb093521cc3d794dc3c5ba75b22
natty-mii <n.miya7@gmail.com>
2020-12-23 17:45:14 +0900

    add username, application_controller, devise.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_pemitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resources)
     items_path
+  end
+
+  protected
+  def configure_pemitted_parameters
+    added_attrs = [ :email, :username, :password, :password_confirmation]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, kyes: added_attrs
   end
 end
